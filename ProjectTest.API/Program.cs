81918d9ee059d98ce8738c3d28ab12d39e01ce25
Stywar <franco111988@gmail.com>
2023-04-03 21:12:11 -0500

    add new file

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -12,7 +12,7 @@ var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 13122022-v1.0.1 CI/CD in KUBERNETES IN AZURE JENKINSFILEV2 v1.0.2 AFORO255";
+    return "GET - Net 6 Running ... 03042023 v1.0.1 FRANCO VARGAS AFORO255";
 });
 
 app.MapPost("/", () =>
